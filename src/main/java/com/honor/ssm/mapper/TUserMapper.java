80fa3aaf9c9c7586fa2f93bedbf3b75c82fdc5b9
daitianci <dtc6323833>
2019-03-07 14:42:09 +0800

    update pom

---
+++ src/main/java/com/honor/ssm/mapper/TUserMapper.java
@@ -0,0 +1,16 @@
+package com.honor.ssm.mapper;
+
+import com.honor.ssm.entity.TUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author daitianci
+ * @since 2019-03-07
+ */
+public interface TUserMapper extends BaseMapper<TUser> {
+
+}
